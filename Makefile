9188046ee8bd0466e0234b6c1267c0af7929306d
Miguel Liezun <liezun.js@gmail.com>
2022-02-28 12:10:11 -0300

    Using libc to handle c strings and test mysql plugin

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
 compile_dylib:
 	cargo build --release
-	
+
 compile_c:
 	gcc -c testprogram/main.c -o testprogram/main.o
 	gcc testprogram/main.o target/release/libblake_udf.dylib -o testprogram/main
+
+test_plugin:
+	docker container rm -f blake-udf
+	docker build -t blake-udf .
+	docker run --name blake-udf -e MYSQL_ROOT_PASSWORD=testpass -d blake-udf
+	docker exec blake-udf sh -c 'mysql -uroot -ptestpass -e "create function blake3_hash returns string soname \"libblake_udf.so\";"'
+	docker exec blake-udf sh -c 'mysql --binary-as-hex=0 -uroot -ptestpass -e "select blake3_hash(\"a\");"'
 
 clean:
 	rm -rf target outputs
--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,8 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+use libc::{strcpy, strlen};
 use std::ffi::CString;
-use std::slice;
-
-unsafe fn write_message(message_ptr: *mut ::std::os::raw::c_char, message: String) -> u64 {
-    let c_message = match CString::new(message) {
-        Ok(cs) => cs,
-        Err(_) => return 0, // failed to convert to C string
-    };
-    let bytes = c_message.as_bytes_with_nul();
-
-    let message_bytes = slice::from_raw_parts_mut(message_ptr as *mut u8, bytes.len() as usize);
-    message_bytes[..bytes.len()].copy_from_slice(bytes);
-    return bytes.len() as u64;
-}
 
 #[no_mangle]
 pub unsafe extern "C" fn blake3_hash_init(
@@ -26,10 +14,15 @@ pub unsafe extern "C" fn blake3_hash_init(
     message: *mut ::std::os::raw::c_char,
 ) -> bool {
     if (*args).arg_count != 1 {
-        write_message(message, String::from("blake3_hash must have one argument"));
+        strcpy(
+            message,
+            CString::new("blake3_hash must have one argument")
+                .unwrap_or_default()
+                .as_ptr(),
+        );
         return true;
     }
-    *(*args).arg_type = Item_result_STRING_RESULT;
+    *((*args).arg_type) = Item_result_STRING_RESULT;
     (*initid).maybe_null = true;
     return false;
 }
@@ -45,9 +38,8 @@ pub unsafe extern "C" fn blake3_hash(
 ) -> *mut ::std::os::raw::c_char {
     let text = *((*args).args);
 
-    let res = String::from(CString::from_raw(text).to_str().unwrap_or_default());
-
-    *res_length = write_message(result, res);
+    strcpy(result, text);
+    *res_length = strlen(text) as u64;
 
     return result;
 }
