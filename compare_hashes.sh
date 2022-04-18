6895de6d0de037f594fff23992a4f125dd4640f0
Miguel Liezun <liezun.js@gmail.com>
2022-04-18 20:14:30 -0300

    Reduce mysql warmup wait time by half

--- compare_hashes.sh
+++ compare_hashes.sh
@@ -9,7 +9,7 @@ MYSQL_ROOT_PASSWORD="testpass"
 docker container rm -f blake-udf
 docker build -t blake-udf .
 docker run --name blake-udf -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d blake-udf
-sleep 20 # Mysql takes time to start
+sleep 10 # Mysql takes time to start
 
 # Configure blake3_hash function in mysql
 docker exec blake-udf sh -c "mysql -u$MYSQL_USER -p$MYSQL_ROOT_PASSWORD -e 'create function blake3_hash returns string soname \"libblake_udf.so\";'"
